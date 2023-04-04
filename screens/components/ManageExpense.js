724c8a5aa933b255bc06ec3d8e2847b9c00d6b03
kumarmudur <kumarmudur10@gmail.com>
2023-04-04 06:56:53 +0530

    [React-Native][Screens] manage expense component changes

--- screens/components/ManageExpense.js
+++ screens/components/ManageExpense.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {useLayoutEffect} from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+
+import IconButton from '../../components/ui/IconButton';
+import {GlobalStyles} from '../../constants/styles';
 
 const ManageExpense = ({route, navigation}) => {
   const editedExpenseId = route.params?.expenseId;
@@ -12,7 +15,37 @@ const ManageExpense = ({route, navigation}) => {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpense Screen</Text>;
+  const deleteExpenseHandler = () => {};
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
