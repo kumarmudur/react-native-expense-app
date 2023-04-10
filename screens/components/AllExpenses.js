1646de5fcaa65dcec799ae1398276eb2b32bb58a
kumarmudur <kumarmudur10@gmail.com>
2023-04-10 06:52:11 +0530

    [React-native] added fallback text in all components

--- screens/components/AllExpenses.js
+++ screens/components/AllExpenses.js
@@ -6,7 +6,11 @@ import {ExpensesContext} from '../../store/expenses-context';
 const AllExpenses = () => {
   const expenseCtx = useContext(ExpensesContext);
   return (
-    <ExpensesOutput expenses={expenseCtx.expenses} expensesPeriod="Total" />
+    <ExpensesOutput
+      expenses={expenseCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found."
+    />
   );
 };
 
--- screens/components/ManageExpense.js
+++ screens/components/ManageExpense.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import {useLayoutEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 
