59c6a8e049c79c369c97fd37b6d8429ef39c9324
kumarmudur <kumarmudur10@gmail.com>
2023-04-11 22:56:41 +0530

    React-Native] manage expense component changes

--- components/expensesOutput/ExpensesOutput.js
+++ components/expensesOutput/ExpensesOutput.js
@@ -1,79 +1,19 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 import {GlobalStyles} from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-9'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.59,
-    date: new Date('2022-1-10'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 10.59,
-    date: new Date('2022-1-15'),
-  },
-  {
-    id: 'e4',
-    description: 'books',
-    amount: 15.29,
-    date: new Date('2021-12-10'),
-  },
-  {
-    id: 'e5',
-    description: 'Laptops',
-    amount: 105.29,
-    date: new Date('2022-1-25'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-9'),
-  },
-  {
-    id: 'e7',
-    description: 'A pair of trousers',
-    amount: 89.59,
-    date: new Date('2022-1-10'),
-  },
-  {
-    id: 'e8',
-    description: 'Some bananas',
-    amount: 10.59,
-    date: new Date('2022-1-15'),
-  },
-  {
-    id: 'e9',
-    description: 'books',
-    amount: 15.29,
-    date: new Date('2021-12-10'),
-  },
-  {
-    id: 'e10',
-    description: 'Laptops',
-    amount: 105.29,
-    date: new Date('2022-1-25'),
-  },
-];
-
-const ExpensesOutput = ({expenses, expensesPeriod}) => {
-  console.log('ExpensesOutput');
+const ExpensesOutput = ({expenses, expensesPeriod, fallbackText}) => {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+  if (expenses.length) {
+    content = <ExpensesList expenses={expenses} />;
+  }
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
     </View>
   );
 };
@@ -87,5 +27,11 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
   },
 });
---
+++ components/manageExpense/ExpenseForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ExpenseForm = () => {
+  console.log('Expense Form');
+  return <View />;
+};
+
+export default ExpenseForm;
