b815393405e8f3918789b718ea939cd6847ad43b
kumarmudur <kumarmudur10@gmail.com>
2023-04-18 22:27:28 +0530

    [React-Native] expense form input changes

--- components/manageExpense/Input.js
+++ components/manageExpense/Input.js
@@ -3,14 +3,14 @@ import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 import {GlobalStyles} from '../../constants/styles';
 
-const Input = ({label, textInputConfig}) => {
+const Input = ({label, style, textInputConfig}) => {
   let inputStyles = [styles.input];
 
   if (textInputConfig && textInputConfig.multiline) {
     inputStyles.push(styles.inputMultiline);
   }
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginHorizontal: 4,
     marginVertical: 8,
+    flex: 1,
   },
   label: {
     fontSize: 12,
